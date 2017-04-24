f3c54a5c01816e3c2aa73201b7ff7b71a9c0b12b
wuliwei9278 <wuliwei9278@gmail.com>
2017-04-23 20:56:12 -0700

    changes back

--- kernel.cpp
+++ kernel.cpp
@@ -18,45 +18,16 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 #include "kernel.h"
+#include <hal/uart.h>
 
 static const char FromKernel[] = "kernel";
 
-
-
-class KMultiCoreSched : public CMultiCoreSupport
-{
-public:
-    KMultiCoreSched (CLogger *m_Logger, CMemorySystem *pMemorySystem){
-
-    }
-    ~KMultiCoreSched (void){
-
-    }
-
-    boolean Initialize (void)   { return TRUE; }
-
-    void Run (unsigned nCore) {
-        //scheduler stuff
-        //start scheduling
-        
-    }
-
-private:
-
-private:
-    CLogger *m_Logger;
-};
-
-
 CKernel::CKernel (void)
-:	m_Screen (m_Options.GetWidth (), m_Options.GetHeight ()),
-	m_Timer (&m_Interrupt),
-	m_Logger (m_Options.GetLogLevel (), &m_Timer)
+:   m_Screen (m_Options.GetWidth (), m_Options.GetHeight ()),
+    m_Timer (&m_Interrupt),
+    m_Logger (m_Options.GetLogLevel (), &m_Timer)
 {
-	m_ActLED.Blink (5);	// show we are alive
-    
-    for (int i = 0; i < CORES; ++i)
-        m_MultiCoreSched[i] = new KMultiCoreSched(&m_Logger, &m_Memory);
+    m_ActLED.Blink (5); // show we are alive
 }
 
 CKernel::~CKernel (void)
@@ -65,113 +36,81 @@ CKernel::~CKernel (void)
 
 boolean CKernel::Initialize (void)
 {
-	boolean bOK = TRUE;
+    boolean bOK = TRUE;
 
-	if (bOK)
-	{
-		bOK = m_Screen.Initialize ();
-	}
-
-	if (bOK)
-	{
-		bOK = m_Serial.Initialize (115200);
-	}
-
-	if (bOK)
-	{
-		CDevice *pTarget = m_DeviceNameService.GetDevice (m_Options.GetLogDevice (), FALSE);
-		if (pTarget == 0)
-		{
-			pTarget = &m_Screen;
-		}
-
-		bOK = m_Logger.Initialize (&m_Serial);
-	}
-
-	if (bOK)
-	{
-		bOK = m_Interrupt.Initialize ();
-	}
-
-	if (bOK)
-	{
-		bOK = m_Timer.Initialize ();
-	}
     if (bOK)
     {
-        for (int i = 0; i < CORES && bOK; ++i)
-          bOK = m_MultiCoreSched[i].Initialize ();   // must be initialized at last
+        bOK = m_Screen.Initialize ();
     }
-	return bOK;
+
+    if (bOK)
+    {
+        bOK = m_Serial.Initialize (115200);
+    }
+
+    if (bOK)
+    {
+        CDevice *pTarget = m_DeviceNameService.GetDevice (m_Options.GetLogDevice (), FALSE);
+        if (pTarget == 0)
+        {
+            pTarget = &m_Screen;
+        }
+
+        bOK = m_Logger.Initialize (pTarget);
+    }
+
+    if (bOK)
+    {
+        bOK = m_Interrupt.Initialize ();
+    }
+
+    if (bOK)
+    {
+        bOK = m_Timer.Initialize ();
+    }
+
+  uart_init();
+
+    return bOK;
 }
-
-#if 1
-
-#include "thread.cpp"
-
-#endif
-
-static thread_t s_thread;
-static thread_t main_thread;
-
-//maybe let scheduler setup the first thread(main)'s context
-//this is used for finding the TCB/Thread during scheduling
-static thread_t *RUNNING_THREAD_ON_CORE[1] = {&main_thread};
-
-static TCB main_tcb = TCB();
-
 
 TShutdownMode CKernel::Run (void)
 {
-	m_Logger.Write (FromKernel, LogNotice, "Compile time: " __DATE__ " " __TIME__);
-        //thread_create(&(threads[0]),&ThreadWorker,5,this);
-        thread_t * thread = &s_thread;
+    m_Logger.Write (FromKernel, LogNotice, "Compile time: " __DATE__ " " __TIME__);
 
-        TCB *tcb = new TCB();
-        thread->tcb = tcb;
-        tcb->stack_size = TASK_STACK_SIZE;
-        tcb->stack  =  new u8[TASK_STACK_SIZE]; //new Stack();
-        memset (&(tcb->regs), 0, sizeof(tcb->regs));
-        //stack grow downward
-        tcb->regs.sp = (size_t) tcb->stack + TASK_STACK_SIZE;
-        tcb->regs.lr = (size_t) stub;
+    m_Logger.Write (FromKernel, LogNotice, "An exception will occur after 15 seconds from now");
 
-        tcb->regs.r0 = (size_t) ThreadWorker;
-        tcb->regs.r1 = (size_t) 5;
-        tcb->regs.r2 = (size_t) this;
-        tcb->state = THREAD_READY;
+    // start timer to elapse after 15 seconds
+    m_Timer.StartKernelTimer (15 * HZ, TimerHandler);
 
-        //thread_yield part 
-        //thread_t *curThread = RUNNING_THREAD_ON_CORE[CMultiCoreSupport::ThisCore()];
-        //&(curThread->tcb->regs)
-        TaskSwitch(&(main_tcb.regs),&(tcb->regs)); //SaveLoadSwitch
+    // generate a log message every second
+    unsigned nTime = m_Timer.GetTime ();
+    while (1)
+    {
+        while (nTime == m_Timer.GetTime ())
+        {
+            // just wait a second
+        }
 
-        long exitValue = 1;//thread_join(&(threads[0]));
-        m_Logger.Write (FromKernel, LogNotice,"Thread %d returned with %ld\n",0,exitValue);
-	return ShutdownHalt;
+        nTime = m_Timer.GetTime ();
+
+        m_Logger.Write (FromKernel, LogNotice, "Time is %u", nTime);
+
+    uart_puts("Time is here\r");
+    }
+
+    return ShutdownHalt;
 }
-
-void 
-CKernel::ThreadWorker(int i, void *ker){
-    CKernel *kernel = (CKernel *) ker;
-    kernel->m_Logger.Write (FromKernel, LogNotice,"Hello from thread %d \n",i);    
-    TaskSwitch(&(s_thread.tcb->regs),&(main_tcb.regs));
-    //thread_exit(100+i);
-}
-
-
-
 
 void CKernel::TimerHandler (unsigned hTimer, void *pParam, void *pContext)
 {
-    #if 1
-    	// jump to an invalid address (execution is only allowed below _etext, see circle.ld)
-    	void (*pInvalid) (void) = (void (*) (void)) 0x500000;
+#if 1
+    // jump to an invalid address (execution is only allowed below _etext, see circle.ld)
+    void (*pInvalid) (void) = (void (*) (void)) 0x500000;
 
-    	(*pInvalid) ();
-    #else
-    	// alternatively execute an undefined instruction
-    	asm volatile (".word 0xFF000000");
-    #endif
+    (*pInvalid) ();
+#else
+    // alternatively execute an undefined instruction
+    asm volatile (".word 0xFF000000");
+#endif
 }
-
